c37b56ba7e7d47084f15f6604436e929a894519b
giovani quimelli <giovaniquimelli@hotmail.com>
2023-06-07 23:35:44 +0900

    Add role text on overhead widget

--- Source/Shooter/GameMode/LobbyGameMode.cpp
+++ Source/Shooter/GameMode/LobbyGameMode.cpp
@@ -9,7 +9,9 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 	Super::PostLogin(NewPlayer);
 
 	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
-	if (NumberOfPlayers == 2)
+	if (NumberOfPlayers == 1
+		
+		)
 	{
 		UWorld* World = GetWorld();
 		if (World)
